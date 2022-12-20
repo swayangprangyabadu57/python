f0c7868ea8c82189af2037f55030230141a1558f
swayangprangyabadu57 <91285381+swayangprangyabadu57@users.noreply.github.com>
2022-12-20 09:56:20 +0530

    Add files via upload

---
+++ first_task/task_1.py
@@ -0,0 +1,23 @@
+x=[["data","science","machine","learning"],
+      "data science",["analytcs",30.6],"data science","computer vision"]
+print(x)
+
+print(x[0][-2]+x[4][-6:])
+
+print(x[0][-2]+" "+x[0][-3]) 
+print(" ")
+
+
+print(x[2][-2]+x[0][-3])
+
+print(x[4][-6:]+ str(30.6))
+
+print(x[0][-4]+x[0][-1])
+
+print(x[1][-7:]+x[0][-1])
+
+print(x[0][-4]+x[2][-2])
+
+print(str(30.6)+x[0][0])
+
+print(x[0][0]+x[0][-1])
